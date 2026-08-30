464170a3c9c9fb5257907cceb848eef85afa369c
Audun Seeberg <60737741+audunseeberg@users.noreply.github.com>
2026-08-30 12:30:16 +0200

    keymap updates

---
+++ config/key_positions.h
@@ -0,0 +1,57 @@
+/*
+ * Corne 3x6+3 key positions for combo definitions.
+ *
+ *  0   1   2   3   4   5       6   7   8   9  10  11
+ * 12  13  14  15  16  17      18  19  20  21  22  23
+ * 24  25  26  27  28  29      30  31  32  33  34  35
+ *                 36  37  38  39  40  41
+ *
+ * Names below refer to the physical position's Base-layer legend.
+ */
+#pragma once
+
+#define POS_TAB   0
+#define POS_Q     1
+#define POS_W     2
+#define POS_E     3
+#define POS_R     4
+#define POS_T     5
+#define POS_Y     6
+#define POS_U     7
+#define POS_I     8
+#define POS_O     9
+#define POS_P     10
+#define POS_BSPC  11
+
+#define POS_LCTRL 12
+#define POS_A     13
+#define POS_S     14
+#define POS_D     15
+#define POS_F     16
+#define POS_G     17
+#define POS_H     18
+#define POS_J     19
+#define POS_K     20
+#define POS_L     21
+#define POS_SEMI  22
+#define POS_SQT   23
+
+#define POS_LSHFT 24
+#define POS_Z     25
+#define POS_X     26
+#define POS_C     27
+#define POS_V     28
+#define POS_B     29
+#define POS_N     30
+#define POS_M     31
+#define POS_COMMA 32
+#define POS_DOT   33
+#define POS_FSLH  34
+#define POS_ESC   35
+
+#define POS_LGUI  36
+#define POS_LOWER 37
+#define POS_SPACE 38
+#define POS_RET   39
+#define POS_RAISE 40
+#define POS_RALT  41
